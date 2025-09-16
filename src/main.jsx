e755a6d7247ff482588d8dae52b614ce427f209f
Joao Gouveia <joaogouveia.work@gmail.com>
2025-09-16 20:30:49 +0100

    changes 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Work from "./pages/work.jsx";
+// import Work from "./pages/Work.jsx";
 import "./assets/scss/main.scss";
 import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./App.jsx";
-import ContactPage from "./pages/ContactPage.jsx";
-import AboutMe from "./pages/AboutMe.jsx";
+// import ContactPage from "./pages/ContactPage.jsx";
+// import AboutMe from "./pages/AboutMe.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/AboutMe" element={<AboutMe />} />
-        <Route path="/Work" element={<Work />} />
-        <Route path="/ContactPage" element={<ContactPage />} />
+        {/* <Route path="/AboutMe" element={<AboutMe />} /> */}
+        {/* <Route path="/Work" element={<Work />} />
+        <Route path="/ContactPage" element={<ContactPage />} /> */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
